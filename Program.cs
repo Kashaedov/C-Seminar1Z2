66b2143ce638d54a133e283ebc94f446d0ccda6e
Kashaedov Vitaliy <Vitalickashaedov@mail.ru>
2022-06-30 18:22:01 +0300

    задача2 итоговая

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
-﻿// числа не равны друг другу
+﻿
 Console.WriteLine ("Введите первое число");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine ("Введите второе число");
 int number2 = Convert.ToInt32(Console.ReadLine());
-if (number1>number2)
+ if (number1 == number2) Console.WriteLine ($"Число  {number1} равно {number2} " );
+else if (number1>number2)
 {
     Console.WriteLine ($"Число {number1} больше числа {number2} " );
 }
@@ -11,8 +12,5 @@ else
 {
     Console.WriteLine ($"Число {number2} больше числа {number1} " );
 }
-// Как исправить, если числа одинаковые?
-//if (number1 == number2)
-//{
-//    Console.WriteLine ($"Число  {number1} равно {number2} " );
-//}
+
+
